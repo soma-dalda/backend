0e5dc0ac0d890cbf6ebc6db1cf2e0e611ac6a33b
jonghyukleee <jh5253krkr@naver.com>
2022-07-26 15:23:26 +0900

    🎉 프로젝트 생성

---
+++ src/main/java/shop/dalda/DaldaApplication.java
@@ -0,0 +1,13 @@
+package shop.dalda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaldaApplication.class, args);
+	}
+
+}
