aaac8f6139a4ed9052657d82bdebd93dfb27b13c
jonghyukleee <jh5253krkr@naver.com>
2022-09-05 16:04:26 +0900

    "불필요한 코드 정리"

--- src/main/java/shop/dalda/DaldaApplication.java
+++ src/main/java/shop/dalda/DaldaApplication.java
@@ -8,8 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class DaldaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DaldaApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DaldaApplication.class, args);
+    }
 }
