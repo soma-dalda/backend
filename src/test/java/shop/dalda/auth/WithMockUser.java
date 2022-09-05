13d8374d4a7a79dbf7517bc3b0984fa884ac1abe
jonghyukleee <jh5253krkr@naver.com>
2022-09-05 15:59:33 +0900

    "MockUser 수정"

--- src/test/java/shop/dalda/auth/WithMockUser.java
+++ src/test/java/shop/dalda/auth/WithMockUser.java
@@ -1,13 +1,10 @@
 package shop.dalda.auth;
 
 
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.test.context.support.WithSecurityContext;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.util.ArrayList;
-import java.util.List;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
