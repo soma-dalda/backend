7d14cc240156eb3a04b55ed6b3cefcb91d8272a9
Kim Minseong <alstjd98522@gmail.com>
2022-09-07 17:41:36 +0900

    [DD-71] 이미지 업로드 API Swagger 연동
    이미지 업로드 API 메서드의 기능 및 파라미터 설명 추가 For Swagger

--- src/main/java/shop/dalda/s3/S3UploadController.java
+++ src/main/java/shop/dalda/s3/S3UploadController.java
@@ -1,5 +1,7 @@
 package shop.dalda.s3;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +19,9 @@ public class S3UploadController {
 
     private final S3UploadService s3UploadService;
 
+    @Operation(summary = "이미지 업로드", description = "이미지 등록을 요청하면 S3에 이미지를 등록 후 그에 해당하는 url 반환하는 메서드")
     @PostMapping("/upload")
-    public ResponseEntity<S3ResponseDto> uploadFile(@RequestParam("image") MultipartFile multipartFile) throws IOException {
+    public ResponseEntity<S3ResponseDto> uploadFile(@Parameter(description = "업로드할 이미지 파일") @RequestParam("image") MultipartFile multipartFile) throws IOException {
         String uploadUrl = s3UploadService.upload(multipartFile.getInputStream(), multipartFile.getOriginalFilename(), multipartFile.getSize());
         S3ResponseDto s3ResponseDto = S3ResponseDto.builder().url(uploadUrl).build();
 
