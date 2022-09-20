a3550a97991aae543681ea9a1f7d89c55b0fda23
Kim Minseong <alstjd98522@gmail.com>
2022-09-20 17:42:16 +0900

    Order Entity @Getter 레벨 변경
    - Order 엔티티의 @Getter를 클래스 레벨에 붙여 가독성 개선

--- src/main/java/shop/dalda/order/Order.java
+++ src/main/java/shop/dalda/order/Order.java
@@ -15,55 +15,45 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Getter
 @Table(name = "orders")
 public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "company_id")
-    @Getter
     private User company;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "consumer_id")
-    @Getter
     private User consumer;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "template_id")
-    @Getter
     private Template template;
 
     @Column(name = "img_url")
-    @Getter
     private String image;
 
     @Convert(converter = JSONConverter.class)
     @Column(columnDefinition = "json")
-    @Getter
     private List<Answer> templateResponses;
 
     @Column(name = "order_date")
-    @Getter
     private LocalDateTime orderDate;
 
     @Column(name = "pickup_date")
-    @Getter
     private LocalDateTime pickupDate;
 
     @Column(name = "pickup_notice_phone")
-    @Getter
     private String pickupNoticePhone;
 
     @Column(name = "order_status")
-    @Getter
     private OrderStatus orderStatus;
 
     @Column(name = "status_change_date")
-    @Getter
     private LocalDateTime statusChangeDate;
 }
