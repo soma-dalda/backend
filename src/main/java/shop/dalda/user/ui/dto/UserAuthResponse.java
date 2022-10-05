60367f8b39a8ff5d398f2738af2c2bf0d298a798
jonghyukleee <jh5253krkr@naver.com>
2022-10-05 15:58:26 +0900

    [DD-103] 유저정보 조회 API 수정

--- src/main/java/shop/dalda/user/ui/dto/UserAuthResponse.java
+++ src/main/java/shop/dalda/user/ui/dto/UserAuthResponse.java
@@ -2,22 +2,41 @@ package shop.dalda.user.ui.dto;
 
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Data;
+import shop.dalda.user.domain.BusinessHour;
+import shop.dalda.user.domain.CompanyLink;
 import shop.dalda.user.domain.Role;
+
+import java.util.List;
 
 @Data
 public class UserAuthResponse {
     Long id;
     String username;
     String userPhone;
-    String role;
-    String CompanyDomain;
+    Role role;
+    String companyName;
+    String companyIntroduction;
+    String companyLocation;
+    String companyDomain;
+    List<BusinessHour> businessHours;
+    String qnaLink;
+    String instaLink;
+    List<CompanyLink> etcLinks;
 
     @QueryProjection
-    public UserAuthResponse(Long id, String username, String userPhone, Role role, String companyDomain) {
+
+    public UserAuthResponse(Long id, String username, String userPhone, Role role, String companyName, String companyIntroduction, String companyLocation, String companyDomain, List<BusinessHour> businessHours, String qnaLink, String instaLink, List<CompanyLink> etcLinks) {
         this.id = id;
         this.username = username;
         this.userPhone = userPhone;
-        this.role = role.getRole();
-        this.CompanyDomain = companyDomain;
+        this.role = role;
+        this.companyName = companyName;
+        this.companyIntroduction = companyIntroduction;
+        this.companyLocation = companyLocation;
+        this.companyDomain = companyDomain;
+        this.businessHours = businessHours;
+        this.qnaLink = qnaLink;
+        this.instaLink = instaLink;
+        this.etcLinks = etcLinks;
     }
 }
