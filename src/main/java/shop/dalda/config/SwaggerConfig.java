2a85d1f62c1d08a53e4542c609ca9ad84b8d34c5
jonghyukleee <jh5253krkr@naver.com>
2022-10-13 20:06:51 +0900

    [DD-109] Swagger 수정

--- src/main/java/shop/dalda/config/SwaggerConfig.java
+++ src/main/java/shop/dalda/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -14,10 +17,16 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        // Info
         Info info = new Info().title("Dalda API 명세서")
                 .description("Dalda API 명세서 입니다")
                 .version("v1");
-            io.swagger.v3.oas.models.security.SecurityScheme securityScheme =
+        // Server
+        List<Server> servers = List.of(new Server()
+                .url("https://dev.dalda.shop")
+                .description("https request"));
+
+        io.swagger.v3.oas.models.security.SecurityScheme securityScheme =
                     new io.swagger.v3.oas.models.security.SecurityScheme()
                             .type(io.swagger.v3.oas.models.security.SecurityScheme.Type.HTTP)
                             .scheme("bearer")
@@ -27,6 +36,7 @@ public class SwaggerConfig {
             SecurityRequirement securityRequirement = new SecurityRequirement().addList("bearerAuth");
 
             return new OpenAPI()
+                    .servers(servers)
                     .components(new Components().addSecuritySchemes("bearerAuth",securityScheme))
                     .security(List.of(securityRequirement))
                     .info(info);
