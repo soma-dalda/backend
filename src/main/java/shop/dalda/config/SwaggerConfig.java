d7744c7a5740eedb7b7a24434131a2aa3176bbd4
jonghyukleee <jh5253krkr@naver.com>
2022-10-13 20:11:02 +0900

    [DD-109] Swagger 수정

--- src/main/java/shop/dalda/config/SwaggerConfig.java
+++ src/main/java/shop/dalda/config/SwaggerConfig.java
@@ -8,8 +8,6 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 @Configuration
