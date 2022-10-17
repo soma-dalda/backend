c260595411a5362a79a16d24661e4635a89277b6
Kim Minseong <alstjd98522@gmail.com>
2022-10-17 22:08:49 +0900

    [DD-106] ContentFactory를 통한 Content 생성
    - Content 추상 클래스 구현
    - ContentFactory를 통해 type에 맞는 구현체 생성

--- src/main/java/shop/dalda/template/domain/content/ContentFactory.java
+++ src/main/java/shop/dalda/template/domain/content/ContentFactory.java
@@ -1,15 +1,17 @@
 package shop.dalda.template.domain.content;
 
+import org.json.simple.JSONObject;
 import shop.dalda.exception.template.UnsupportedTypeContentException;
 
 import java.util.Objects;
 
 public class ContentFactory {
-    public static Content create(String type) {
+    public static Content create(JSONObject jsonObject) {
+        String type = (String) jsonObject.get("type");
         if (Objects.equals(type, "singleObjective") || Objects.equals(type, "multiObjective"))
-            return new ObjectiveContent();
+            return new ObjectiveContent(jsonObject);
         else if (Objects.equals(type, "shortSubjective") || Objects.equals(type, "multiSubjective"))
-            return new SubjectiveContent();
+            return new SubjectiveContent(jsonObject);
         else
             throw new UnsupportedTypeContentException();
     }
