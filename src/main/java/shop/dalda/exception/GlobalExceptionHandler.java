fcf6e6b2e4fd6d7ddcb96e70e48a910cdd665469
Kim Minseong <alstjd98522@gmail.com>
2022-10-17 22:18:09 +0900

    GlobalExceptionHandler Exception 추가
    - 기존에 추가되지 않았던 Exception 처리 추가
    - Template 리팩토링으로 추가된 Exception 추가

--- src/main/java/shop/dalda/exception/GlobalExceptionHandler.java
+++ src/main/java/shop/dalda/exception/GlobalExceptionHandler.java
@@ -5,9 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import shop.dalda.exception.order.OrderNotBelongToUserException;
+import shop.dalda.exception.order.OrderNotFoundException;
 import shop.dalda.exception.template.TemplateInvalidException;
 import shop.dalda.exception.template.TemplateNotBelongToUserException;
 import shop.dalda.exception.template.TemplateNotFoundException;
+import shop.dalda.exception.template.UnsupportedTypeContentException;
 
 @Slf4j
 @RestControllerAdvice
@@ -15,20 +18,13 @@ public class GlobalExceptionHandler {
 
     private static final String LOG_FORMAT = "Class: {}, Code: {}, Message: {}";
 
+    // Template 관련 Exception
     @ExceptionHandler(TemplateInvalidException.class)
     public ResponseEntity<String> templateInvalidException(TemplateInvalidException e) {
         log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
-                .body(e.getErrorCode());
-    }
-
-    @ExceptionHandler(TemplateNotFoundException.class)
-    public ResponseEntity<String> templateNotFoundException(TemplateNotFoundException e) {
-        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST.value())
-                .body(e.getErrorCode());
+                .body(e.getMessage());
     }
 
     @ExceptionHandler(TemplateNotBelongToUserException.class)
@@ -36,6 +32,48 @@ public class GlobalExceptionHandler {
         log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
         return ResponseEntity
                 .status(HttpStatus.BAD_REQUEST.value())
-                .body(e.getErrorCode());
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(TemplateNotFoundException.class)
+    public ResponseEntity<String> templateNotFoundException(TemplateNotFoundException e) {
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(UnsupportedTypeContentException.class)
+    public ResponseEntity<String> UnsupportedTypeContentException(UnsupportedTypeContentException e) {
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(e.getMessage());
+    }
+
+    // Order 관련 Exception
+    @ExceptionHandler(OrderNotBelongToUserException.class)
+    public ResponseEntity<String> OrderNotBelongToUserException(OrderNotBelongToUserException e) {
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(e.getMessage());
+    }
+
+    @ExceptionHandler(OrderNotFoundException.class)
+    public ResponseEntity<String> OrderNotFoundException(OrderNotFoundException e) {
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(e.getMessage());
+    }
+
+    // 공통 Exception
+    @ExceptionHandler(UnknownFormatRequestException.class)
+    public ResponseEntity<String> UnknownFormatRequestException(UnknownFormatRequestException e) {
+        log.warn(LOG_FORMAT, e.getClass().getSimpleName(), e.getErrorCode(), e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(e.getMessage());
     }
 }
